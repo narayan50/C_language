a79b725292f952090b206cf2b88e7cfd504241a5
Narayan Tamatta <narayantamatta@yahoo.com>
2025-12-10 20:24:58 +0530

    operators

---
+++ relationaloperators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    int num1 , num2;
+    printf("enter the value");
+    scanf("%d", &num1);
+    printf("enter second number:");
+    scanf("%d", &num2);
+    // relational operators
+
+    printf("num1 == num2 : %d\n", num1 == num2);
+    printf("num1 !=num2 : %d\n" , num1 !=num2  );
+    printf("num1> num2: %d\n:", num1> num2);
+    printf("num1< num2: %d\n:", num1< num2);
+  printf("num1< num2: %d\n:", num1< num2);
+  
+}
